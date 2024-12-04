7a141474c8c79d0e278bcfc06af37b1d4a958883
Kassidy Wall <wallkassidy@yahoo.com>
2024-12-04 16:51:45 -0700

    third test

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,18 @@
 import behave_webdriver
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome.headless()
-    #context.behave_driver = behave_webdriver.Chrome()
+    # Set up the Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Running in headless mode
+    
+    # You can add a timeout setting directly if needed
+    context.behave_driver = behave_webdriver.Chrome(options=chrome_options)
+    context.behave_driver.set_page_load_timeout(10)
+
+# def before_all(context):
+#     context.behave_driver = behave_webdriver.Chrome.headless(timeout=10)
+#     #context.behave_driver = behave_webdriver.Chrome()
 
 def after_all(context):
     context.behave_driver.quit()
