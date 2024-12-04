77b42320592160da98a5c9becbd242acf4aea35c
Kassidy Wall <wallkassidy@yahoo.com>
2024-12-04 16:23:27 -0700

    first test

--- features/HeronsCalculator.feature
+++ features/HeronsCalculator.feature
@@ -5,5 +5,8 @@ So that I can chat with my math friends like a pro
 
 Scenario: I can calculate the area of a triangle
     Given I open the url "https://byjus.com/herons-calculator/"
-    # write your steps here
-
+    When I set "6" to the inputfield "//*[@id='a']"
+    And I set "8" to the inputfield "//*[@id="b"]"
+    And I set "10" to the inputfield "//*[@id="c"]"
+    And I click on the button "//*[@id="post-948257"]/div[1]/p[5]/input"
+    Then I expect that element "//*[@id='_d']" contains the text "24"
--- test_battery.py
+++ test_battery.py
@@ -16,11 +16,86 @@ def partially_charged_battery():
 
 
 def describe_Battery():
+    
+    def describe_init():
+        
+        def it_assigns_full_capacity_and_charge_attributes(charged_battery, partially_charged_battery):
+            b = charged_battery
+            assert b.mCapacity == 100
+            assert b.mCharge == 100
+        def it_assigns_partial_capacity_and_charge_attributes(charged_battery, partially_charged_battery):
+            b = partially_charged_battery
+            assert b.mCapacity == 100
+            assert b.mCharge == 70
+
+    def describe_getCapacity():
+
+        def it_returns_full_and_partial_capacity(charged_battery, partially_charged_battery):
+            b = charged_battery
+            assert b.getCapacity() == 100
+            bp = partially_charged_battery
+            assert bp.getCapacity() == 100
+
+    def describe_getCharge():
+        
+        def it_returns_full_and_partial_charge(charged_battery, partially_charged_battery):
+            b = charged_battery
+            assert b.getCharge() == 100
+            bp = partially_charged_battery
+            assert bp.getCharge() == 70
 
     def describe_recharge():
-        # your test cases here
-
+        
+        def it_recharges_valid_amount(charged_battery, partially_charged_battery):
+            b = partially_charged_battery
+            b.recharge(10)
+            assert b.getCharge() == 80
+        def it_caps_recharge_amount_at_capacity(charged_battery, partially_charged_battery):
+            b = partially_charged_battery
+            b.recharge(40)
+            assert b.getCharge() == 100
+        def it_does_not_use_negative_charge_amount(charged_battery, partially_charged_battery):
+            b = partially_charged_battery
+            b.recharge(-20)
+            assert b.getCharge() == 70
+        def it_does_not_charge_if_capacity_equals_charge(charged_battery, partially_charged_battery):
+            b = charged_battery
+            b.recharge(5)
+            assert b.getCharge() == 100
+        
+        def it_does_mock_stuff_for_external_monitor(mocker,partially_charged_battery):
+            mock_monitor = Mock()
+            mock_notify_recharge = mocker.patch.object(mock_monitor, 'notify_recharge', return_value=None)
+            b = Battery(100, mock_monitor)
+            b.mCharge = 60
+            b.recharge(5)
+            mock_notify_recharge.assert_called_once()
+        
     def describe_drain():
-        # your test cases here
+        
+        def it_drains_valid_amount(charged_battery, partially_charged_battery):
+            b = charged_battery
+            b.drain(5)
+            assert b.getCharge() == 95
+        def it_does_not_drain_on_zero_charge(charged_battery, partially_charged_battery):
+            b = partially_charged_battery
+            b.drain(70)
+            assert b.getCharge() == 0
+            b.drain(20)
+            assert b.getCharge() == 0
+        def it_does_not_drain_negative_amount(charged_battery, partially_charged_battery):
+            b = partially_charged_battery
+            b.drain(-20)
+            assert b.getCharge() == 70
+        
+        def it_does_mock_stuff_for_external_monitor(mocker,partially_charged_battery):
+            mock_monitor = Mock()
+            mock_notify_drain = mocker.patch.object(mock_monitor, 'notify_drain', return_value=None)
+            b = Battery(100, mock_monitor)
+            b.drain(5)
+            mock_notify_drain.assert_called_once()
+
+
+
 
 
