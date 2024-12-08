db72c5dbb5b72a297f8e94ae09ef68307cfc10fc
Kassidy Wall <wallkassidy@yahoo.com>
2024-12-08 13:11:37 -0700

    trying to get behave to work in headless mode

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,8 @@ import behave_webdriver
 
 def before_all(context):
     context.behave_driver = behave_webdriver.Chrome.headless()
+    chrome_options = behave_webdriver.ChromeOptions()
+    chrome_options.add_argument('window-size=1920x1080')
     #context.behave_driver = behave_webdriver.Chrome()
 
 def after_all(context):
