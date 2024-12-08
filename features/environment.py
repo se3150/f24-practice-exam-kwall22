c5ffdaf9dee94e9e9573ef176505fe9c05e5c686
Kassidy Wall <wallkassidy@yahoo.com>
2024-12-08 13:15:26 -0700

    trying to set window size in headless mode

--- features/environment.py
+++ features/environment.py
@@ -2,8 +2,9 @@ import behave_webdriver
 
 def before_all(context):
     context.behave_driver = behave_webdriver.Chrome.headless()
-    chrome_options = behave_webdriver.ChromeOptions()
-    chrome_options.add_argument('window-size=1920x1080')
+    context.behave_driver.set_window_size(1920,1080)
+    #chrome_options = behave_webdriver.ChromeOptions()
+    #chrome_options.add_argument('window-size=1920x1080')
     #context.behave_driver = behave_webdriver.Chrome()
 
 def after_all(context):
